33f4b0966560e9b5d2b3fc33f6048d69bc73f89d
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 12:48:09 +0000

    Initial commit

---
+++ app/HomeActivity.kt
@@ -0,0 +1,10 @@
+package com.example.loginpage
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+     fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.loginpage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etUsername = findViewById<EditText>(R.id.et_username)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+            } else if (username == "admin" && password == "admin") {
+                val intent = Intent(this, HomeActivity1::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
